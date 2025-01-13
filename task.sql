48813568262117996b646a35572fb13f314da53f
Yuriy.Khilko <yurakhilko@gmail.com>
2025-01-13 17:07:43 +0200

    added fixed 6

--- task.sql
+++ task.sql
@@ -45,7 +45,7 @@ INSERT INTO Products (id,Name)
 
 INSERT INTO Warehouses (id, Name,Address,CountryID)
 	VALUES (1,'Warehouse-1','City-1, Street-1',1);
-INSERT INTO Warehouses (Name,Address,CountryID)
+INSERT INTO Warehouses (id, Name,Address,CountryID)
 	VALUES (2, 'Warehouse-2','City-2, Street-2',2);
 
 INSERT INTO ProductInventory (ProductId,WarehouseId, WarehouseAmount)
