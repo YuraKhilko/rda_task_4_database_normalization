10a7e09062f0b7732bf66ca3df6af1539dbee022
Yuriy.Khilko <yurakhilko@gmail.com>
2025-01-13 16:39:51 +0200

    added solution

--- task.sql
+++ task.sql
@@ -4,19 +4,32 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+CREATE TABLE Warehouses (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    Address VARCHAR(50), 
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,    
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE ProductInventory (
+    ID INT AUTO_INCREMENT,
+    ProductId INT,
+    WarehouseId INT,
+    WarehouseAmount INT,
+	FOREIGN KEY (ProductId) REFERENCES Products(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (WarehouseId) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -26,8 +39,17 @@ INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+
+INSERT INTO Products (Name)
+	VALUES ('AwersomeProduct');
+
+INSERT INTO Warehouses (Name,Address,CountryID)
+	VALUES ('Warehouse-1','City-1, Street-1',1);
+INSERT INTO Warehouses (Name,Address,CountryID)
+	VALUES ('Warehouse-2','City-2, Street-2',2);
+
+INSERT INTO ProductInventory (ProductId,WarehouseId, WarehouseAmount)
+	VALUES (1,1,2);
+INSERT INTO ProductInventory (ProductId,WarehouseId, WarehouseAmount)
+	VALUES (1,2,5);
+        
